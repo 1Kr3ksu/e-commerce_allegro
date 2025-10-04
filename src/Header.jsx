f8f5e470fcbf736809d533a39913aef1b22e1315
1Kr3ksu <kacperbielawski33@gmail.com>
2025-10-04 15:37:40 +0200

    dodanie ikon strony , css do paska i ikon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,32 +1,68 @@
 import './App.css'
+
 function Header() {
   return (
     <>
       <header>
         <nav className="navbar">
           <div className="navbar-logo">
-            <img src="/allegro.png" alt="Logo" />
+            <img src="/allegro.png" alt="Allegro" />
           </div>
-          <div className='navbar-search'>
-            <input type="text" placeholder="Czego szukasz?" />
-            <label for="category">Wszystkie kategorie</label>
-            <select id="category" name="category">
-              <option value="all">Wszystkie kategorie</option>
-              <option value="electronics">Elektronika</option>
-              <option value="fashion">Moda</option>
-              <option value="home">Dom i ogród</option>
-              <option value="sports">Sport i rekreacja</option>
-              <option value="toys">Zabawki</option>
-              <option value="motors">Motoryzacja</option>
-            </select>
-            <button type="submit">Szukaj</button>
+
+          <div className="navbar-search">
+            <div className="search-container">
+              <input type="text" placeholder="czego szukasz?" className="search-input" />
+              <div className="search-category">
+                <select className="category-select">
+                  <option value="all">Wszystkie kategorie</option>
+                  <option value="electronics">Elektronika</option>
+                  <option value="fashion">Moda</option>
+                  <option value="home">Dom i ogród</option>
+                  <option value="sports">Sport i rekreacja</option>
+                  <option value="toys">Zabawki</option>
+                  <option value="motors">Motoryzacja</option>
+                </select>
+                <span className="material-symbols-outlined category-arrow">
+                  keyboard_arrow_down
+                </span>
+
+              </div>
+              <button type="submit" className="search-button">SZUKAJ</button>
+            </div>
           </div>
-          <div className="navbar-links">
-            <button>Zaloguj się</button>
+
+          <div className="navbar-icons">
+            <div className="icon-item">
+              <span className="material-symbols-outlined delivery-icon">local_shipping</span>
+            </div>
+            <div className="icon-item">
+              <span className="material-symbols-outlined chatsupport-icon">
+                chat_info
+              </span>
+            </div>
+            <div className="icon-item">
+              <span className="material-symbols-outlined favorite-icon">
+                favorite
+              </span>
+            </div>
+            <div className="icon-item">
+              <span className="material-icons notification-icons">notifications</span>
+            </div>
+            <div className="icon-item">
+              <span className="material-icons shopping-icons">shopping_bag</span>
+            </div>
+            <div className="user-menu">
+              <span>Moje Allegro</span>
+              <span className="material-symbols-outlined category-arrow">
+                keyboard_arrow_down
+              </span>
+
+            </div>
           </div>
         </nav>
       </header>
     </>
   )
 }
+
 export default Header
