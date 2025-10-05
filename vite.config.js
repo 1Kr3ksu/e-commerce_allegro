6440f458414230b42cfe9304c492cca3dc72557a
1Kr3ksu <kacperbielawski33@gmail.com>
2025-10-05 20:43:36 +0200

    Add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/e-commerce_allegro/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
